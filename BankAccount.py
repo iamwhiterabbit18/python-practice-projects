b88d9d5d24ce079aef465e9eadc80880696a5756
Gene <mangueragene@gmail.com>
2024-12-15 07:57:30 +0800

    -Refactor display_accounts handle transfer in bank class

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,24 @@
+class NumGenerate:
+    def __init__(self):
+        self.available_numbers = list(range(1000, 6000))
+
+    def get_next_num(self):
+        if self.available_numbers:
+            return self.available_numbers.pop(0)
+        else:
+            raise Exception("No more account numbers available!")
+    def return_num(self, num):
+        if num not in self.available_numbers:
+            self.available_numbers.append(num)
+            self.available_numbers.sort()
+
 class Bank:
     def __init__(self):
         self.accounts_list = []
-        self.table1 = num_generate().num_catalogue[0]
-    # def __g-enerate_custom_id(self):
+        self.num_generator = NumGenerate()
+        self.name = "Genesis Bank"
+    def __str__(self):
+        return f"{self.name}"
         
     def __checkAccount(self, account):
         target = [x for x in self.accounts_list if account == x]
@@ -14,6 +30,7 @@ class Bank:
         checked = self.__checkAccount(account)
         if not checked:
             if action == 'create':
+                account.account_num = self.num_generator.get_next_num()
                 self.accounts_list.append(account)
                 return f"{account.account_holder} has been added."
             elif action == 'remove' or 'find':
@@ -22,43 +39,47 @@ class Bank:
             if action == 'create':
                 return f"{account.account_holder} already exist."
             elif action == 'remove':
+                self.num_generator.return_num(account.account_num)
                 self.accounts_list.remove(account)
                 return f"{account.account_holder} has been removed."
             elif action == 'find':
                 return self.__details(account)
-    def __details(self, account):
-        return (
-                    f"Account name: {account.account_holder}\n"
-                    f"Account number: {account.account_num}\n"
-                    f"Account name: {account.balance}"
-                    )
-    def __assign_num(self, account, action):
-        if action == 'create':
-            num = self.table1[0]
-            account.account_num = self.table1[0]
-            self.table1.remove(self.table1[0])
-            return f"{num}"
-        elif action == 'remove':
-            num = account.account_num
-            self.table1.append(num)
-            return f"{num}"
     def create_account(self, account):
-        self.__assign_num(account, 'create')
         return self.__action_perform(account, 'create')
     def remove_account(self, account):
-        self.__assign_num(account, 'remove')
         return self.__action_perform(account, 'remove')
     def find_account(self, account):
         return self.__action_perform(account, 'find')
+    def transfer(self, amount, source_account, target_account) -> str:
+        if amount <= 0:
+            return f"There must be a certain amount."
+        if source_account.balance < amount:
+            return f"Insuffecient balance to transfer: {amount} from {source_account.account_holder}"
+        
+        source_account.balance -= amount
+        target_account.balance += amount
+        return (
+            f"Transferred {amount} from {source_account.account_holder} to {target_account.account_holder}.\n"
+            f"Source Account Balance: {source_account.balance}\n"
+            f"Target Account Balance: {target_account.balance}"
+        )
     def display_accounts(self):
         for x in self.accounts_list:
-            print(f"{self.__details(x)}\n")
-
+            print(f"{x}\n")
 
 class BankAccount:
-    def __init__(self, account_holder, balance, bank: Bank):
+    def __init__(self, account_holder, balance):
         self.account_holder = account_holder
         self.balance = balance
+    def __str__(self):
+        return (
+                    f"Account name: {self.account_holder}\n"
+                    f"Account number: {self.account_num}\n"
+                    f"Account name: {self.balance}"
+                    )
+    # def __repr__(self):
+    #     return (
+    #                 f"BankAccount(account_holder='{self.account_holder}', balance='{self.balance}')")
 
     def deposit(self, amount) -> str:
         if(amount <= 0):
@@ -78,54 +99,14 @@ class BankAccount:
         )
     def check_balance(self) -> str:
         return f"Current Balance: {self.balance}"
-    def transfer(self, amount, target_account) -> str:
-        if self.balance < amount:
-            return f"Insuffecient balance to transfer: {amount}"
-        target = [x for x in bank.accounts_list if x.account_num == target_account.account_num]
-        if target:
-            self.balance -= amount
-            target_account.deposit(amount)
-            return (
-                f"Transaction Complete\n"
-                f"{amount} had been transfered to {target_account.account_holder}\n"
-                f"Current Balance: {self.balance}"
-            )
-        else:
-            return f"No {target_account.account_holder} account"
-
-class num_generate:
-    def __init__(self):
-        self.combi5 = []
-        # contains all tables with different length of digits
-        self.num_catalogue = []
-        self.combination()
-        self.insert_tables(self.combi5)
-    def combination(self):
-        # for 3125
-        for i in range(6):
-            for j in range(6):
-                for k in range(6):
-                    for l in range(6):
-                        for m in range(6):
-                            self.combi5.append(f"{i}{j}{k}{l}{m}")
-    def insert_tables(self, table):
-        self.num_catalogue.append(table)
-        return self.num_catalogue
-
-
 bank = Bank()
-gene = BankAccount("Gene", 100, bank)
-gio = BankAccount("Gene", 100, bank)
-nico = BankAccount("Nico", 321, bank)
-tet = BankAccount("Tet", 1000, bank)
-print(bank.create_account(gene))
-print(bank.create_account(nico))
-print(bank.create_account(gio))
-print(bank.create_account(tet))
-print(bank.remove_account(nico))
-print(gene.transfer(100, nico))
+gene = BankAccount("Gene", 100)
+gio = BankAccount("Gene", 100)
+nico = BankAccount("Nico", 321)
+tet = BankAccount("Tet", 1000)
+
+bank.create_account(gene)
+bank.create_account(gio)
+bank.create_account(gio)
+bank.transfer(100, gene, gio)
 bank.display_accounts()
-
-
-# Ensure account_number is unique for each BankAccount created.
-# Add error handling for invalid operations, such as withdrawing more than the available balance or attempting to transfer to a non-existent account.
